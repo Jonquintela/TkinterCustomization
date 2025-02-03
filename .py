590a791d1f42e2e56b8682d2430cd49cd359c2e0
Jon Quintela <you@example.com>
2025-02-03 11:09:10 -0600

    started learning and actually understanding code

---
+++ .py
@@ -0,0 +1,18 @@
+"""questions = [
+    {"question": "What is 2 + 2?", "options": ["3", "4", "5"], "answer": "4"},
+    {"question": "What is the capital of France?", "options": ["Paris", "London", "Berlin"], "answer": "Paris"},
+    {"question": "Which planet is known as the Red Planet?", "options": ["Earth", "Mars", "Venus"], "answer": "Mars"}
+]
+
+
+question_index = 0
+q = questions[question_index]
+print(list(enumerate(q["options"])))
+
+pito = ["p" for _ in range(3)]
+print(pito)"""
+
+
+add = lambda x, y: print(x+y)
+
+add(1,2)
